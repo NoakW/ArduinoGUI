ef0f3ea3413e9a43e440b412c6099d7e2e91307d
NoakW <myemail>
2024-10-09 19:31:48 +0200

    v3

--- arduino_gui.py
+++ arduino_gui.py
@@ -10,6 +10,34 @@ import time
 import customtkinter
 
 # Function to send command to Arduino
+# Set appearance and theme
+customtkinter.set_appearance_mode("dark")  # Mode
+customtkinter.set_default_color_theme("dark-blue")  # Theme
+
+rpm_max = 315
+
+arduinoConnected = False;
+try:
+    # Open the serial connection to Arduino, adjust COM port if needed)
+    arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1) 
+    print("Connected to Arduino.")
+    arduinoConnected = True;
+except Exception as e:
+    print(f"Failed to connect to Arduino: {e}")
+    
+def rpm_conv(value):
+    approx_rpm = int(rpm_max * value)
+    if approx_rpm <= 40:
+        approx_rpm = 0
+    return approx_rpm
+    
+def slider_callback(value):
+    approx_rpm = rpm_conv(value)
+    pwm_value = int(value * 255)  # Scale to 0-255 for PWM
+    send_command(f"SET_PWM {pwm_value}")
+    status_label.configure(text=f"RPM: {int(approx_rpm)}")# Send PWM command to Arduino
+
+
 def send_command(command):
     try:
         print(f"Sending command: {command}")
@@ -21,30 +49,16 @@ def send_command(command):
 # Function to handle window closing
 def on_closing(arduinoConnected):
     if arduinoConnected:
+        send_command("SET_PWM 0")#
         arduino.close()  # Close the serial connection
         print("Serial port closed.")
-        window.destroy()  # Close the Tkinter window
+    window.destroy() #Close the Tkinter window
+         
     
-def slider_callback(value):
-    progressbar_1.set(value)
-    pwm_value = int(value * 255)  # Scale to 0-255 for PWM
-    send_command(f"SET_PWM {pwm_value}")  # Send PWM command to Arduino
-
-# Set appearance and theme
-customtkinter.set_appearance_mode("dark")  # Mode
-customtkinter.set_default_color_theme("dark-blue")  # Theme
-# Open the serial connection to Arduino, adjust COM port if needed)
-arduinoConnected = False;
-try:
-    arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1)
-    print("Connected to Arduino.")
-    arduinoConnected = True;
-except Exception as e:
-    print(f"Failed to connect to Arduino: {e}")
 
 # Create the main window
 window = customtkinter.CTk()
-window.geometry("500x350")
+window.geometry("600x550")
 
 # Create a frame for content
 frame = customtkinter.CTkFrame(master=window)
@@ -63,16 +77,14 @@ btn_on_red.pack(pady=10)
 btn_off_red = customtkinter.CTkButton(master=frame, text="Turn OFF Red LED", command=lambda: send_command("LOW_RED"))
 btn_off_red.pack(pady=10)
 
-progressbar_1 = customtkinter.CTkProgressBar(master=window)
-progressbar_1.pack(pady=12,padx=10)
-rpm_slider = customtkinter.CTkSlider(master=window,command=slider_callback,from_=0,to=1)
-rpm_slider.pack(pady=12,padx=10)
+rpm_slider = customtkinter.CTkSlider(master=window, command=slider_callback, from_=0, to=1)
+rpm_slider.pack(pady=12, padx=10)
 
 entry_1 = customtkinter.CTkEntry(master=frame,placeholder_text="HELLO")
 entry_1.pack(pady=12,padx=10)
 
-party_btn = customtkinter.CTkButton(master=frame, text="Party",command=lambda: send_command("PARTY"))
-party_btn.pack(pady=10)
+status_label = customtkinter.CTkLabel(master=frame, text="RPM: 0")
+status_label.pack(pady=12, padx=10)
 
 # Code for closing window
 window.protocol("WM_DELETE_WINDOW", lambda: on_closing(arduinoConnected))
